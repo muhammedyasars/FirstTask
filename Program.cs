5c639fe8872811290a1b73f66825a2cebe008632
muham <muham@YAAZOOZ>
2025-03-19 15:34:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public class Program
+{
+    static bool Prime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+        for (int i = 2; i < Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    static void Main()
+    {
+        while (true)
+        {
+
+
+            Console.WriteLine("enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (Prime(num))
+            {
+                Console.WriteLine(num + " is prime");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not prime");
+
+            }
+        }
+    }
+}
+
